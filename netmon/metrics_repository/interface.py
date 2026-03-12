6cba7131a5fc42b179968a649e0467589d706dbf
Rodion Sarygin <k1ll4h_tv3t@protonmail.com>
2026-03-12 16:33:45 +0700

    feat-24: segregated interface for saving and querying

--- netmon/metrics_repository/interface.py
+++ netmon/metrics_repository/interface.py
@@ -1,17 +1,18 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from collections.abc import Iterable
 from datetime import datetime
+from typing import Protocol
 
 from netmon.entities.base_metric_entry import BaseMetricEntry
 
 
-class MetricsRepository(ABC):
-    """Репозиторий для сохранения метрик."""
-
+class MetricsSaver(Protocol):
     @abstractmethod
     async def save_metrics(self, metrics: Iterable[BaseMetricEntry]) -> None:
         """Сохранить список метрик."""
 
+
+class MetricQuerier(Protocol):
     @abstractmethod
     async def metrics_in_interval(self, interval_start: datetime, interval_stop: datetime) -> Iterable[BaseMetricEntry]:
         """Получить список метрик за выбранный период."""
