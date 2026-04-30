356452494e6f49b58ddd71d6882ecdb5514972eb
Valeriy <valera847926@mail.ru>
2026-04-30 09:02:16 +0700

    Обновил ActiveQUICCollector

--- netmon/collectors/activequic/message.py
+++ netmon/collectors/activequic/message.py
@@ -1,9 +1,7 @@
 from typing import TypedDict
 
 
-# TODO: change to pydantic.BaseModel
 class QUICMessage(TypedDict):
     message_id: str
     reply_to: str | None
     sent_at: float
-    additional_data: dict | None
